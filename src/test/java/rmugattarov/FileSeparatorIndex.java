21b4ac639d500d2bcb3457d3f50784f4db7323c5
rmugattarov <mugattarov@gmail.com>
2016-01-05 19:56:10 +0500

    updates

---
+++ src/test/java/rmugattarov/FileSeparatorIndex.java
@@ -0,0 +1,20 @@
+package rmugattarov;
+
+import org.junit.Test;
+
+import java.io.File;
+
+/**
+ * Created by rmugattarov on 05.01.16.
+ */
+public class FileSeparatorIndex {
+    @Test
+    public void should_find_separator_in_path() {
+        System.out.printf("file separator : %s\n", File.separator);
+        System.out.printf("file separatorChar : %s\n", File.separatorChar);
+        System.out.printf("file pathSeparator : %s\n", File.pathSeparator);
+        System.out.printf("file pathSeparatorChar : %s\n", File.pathSeparatorChar);
+        System.out.printf("code source location : %s\n", this.getClass().getProtectionDomain().getCodeSource().getLocation());
+
+    }
+}
