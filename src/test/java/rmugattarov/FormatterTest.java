a5d4b0a299cb6a1c86c62a4271ea0e7949df1099
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-01-11 11:27:54 +0500

    updates

---
+++ src/test/java/rmugattarov/FormatterTest.java
@@ -0,0 +1,15 @@
+package rmugattarov;
+
+import org.junit.Test;
+
+import java.util.Formatter;
+
+/**
+ * Created by rmugattarov on 11.01.2016.
+ */
+public class FormatterTest {
+    @Test
+    public void should_print_formatted_message() {
+        System.out.println(new Formatter().format("Test args %s, %d", "string arg", 123));
+    }
+}
