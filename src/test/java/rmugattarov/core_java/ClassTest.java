1850ffbdc9e138b2f253307eac2144c7bbc94e9e
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-01-13 17:16:54 +0500

    updates

---
+++ src/test/java/rmugattarov/core_java/ClassTest.java
@@ -0,0 +1,18 @@
+package rmugattarov.core_java;
+
+import org.junit.Test;
+
+/**
+ * Created by rmugattarov on 13.01.2016.
+ */
+public class ClassTest {
+    @Test
+    public void test_classes() {
+        System.out.printf("int.class : %s\n", int.class);
+        System.out.printf("boolean.class : %s\n", boolean.class);
+        System.out.printf("void.class : %s\n", void.class);
+        System.out.printf("Integer.TYPE : %s\n", Integer.TYPE);
+        System.out.printf("Boolean.TYPE : %s\n", Boolean.TYPE);
+        System.out.printf("Void.TYPE : %s\n", Void.TYPE);
+    }
+}
