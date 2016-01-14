9ee2af6ebe7c24ec36055cc3b4a1a232547b3156
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-01-14 15:28:35 +0500

    updates

---
+++ src/test/java/rmugattarov/core_java/dates/EqualsTest.java
@@ -0,0 +1,15 @@
+package rmugattarov.core_java.dates;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+/**
+ * Created by rmugattarov on 14.01.2016.
+ */
+public class EqualsTest {
+    @Test
+    public void test_equals() {
+        System.out.printf("new Date().equals(null) : %b\n", new Date().equals(null));
+    }
+}
