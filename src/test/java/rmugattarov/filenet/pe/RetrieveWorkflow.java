80ee0e4156fcad0ba8807f8b5d8be29b30a209cb
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-02-02 11:56:25 +0500

    updates

--- src/test/java/rmugattarov/filenet/pe/RetrieveWorkflow.java
+++ src/test/java/rmugattarov/filenet/pe/RetrieveWorkflow.java
@@ -1,6 +1,8 @@
 package rmugattarov.filenet.pe;
 
+import filenet.vw.api.VWQueue;
 import filenet.vw.api.VWSession;
+import filenet.vw.api.VWWorkBasket;
 import org.junit.Test;
 
 /**
@@ -10,9 +12,18 @@ public class RetrieveWorkflow {
     @Test
     public void test() {
         VWSession vwSession = new VWSession();
-        vwSession.setBootstrapCEURI("http://172.28.24.184:9080/wsi/FNCEWS40MTOM/");
-        vwSession.logon("cpe_bootstrap@tn.fntst.ru", "o9p0[-]=", "CP01");
-        System.out.println(vwSession.getObjectStoreSymbolicName());
-        vwSession.logoff();
+        try {
+            vwSession = new VWSession();
+            vwSession.setBootstrapCEURI("http://172.28.24.184:9080/wsi/FNCEWS40MTOM/");
+            vwSession.logon("cpe_bootstrap@tn.fntst.ru", "o9p0[-]=", "CP01");
+            VWQueue docQueue = vwSession.getQueue("DocQueue");
+            VWWorkBasket basket = docQueue.fetchWorkBasket("Связывание с Заголовком дела");
+            System.out.println(basket.getName());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            vwSession.logoff();
+        }
     }
 }
