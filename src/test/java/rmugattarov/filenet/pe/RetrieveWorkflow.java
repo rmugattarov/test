be1a1c1395a05c742b2a9f0e1eb369b3d1e015e2
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-02-02 13:17:31 +0500

    updates

--- src/test/java/rmugattarov/filenet/pe/RetrieveWorkflow.java
+++ src/test/java/rmugattarov/filenet/pe/RetrieveWorkflow.java
@@ -1,9 +1,13 @@
 package rmugattarov.filenet.pe;
 
+import com.filenet.api.constants.PropertyNames;
 import filenet.vw.api.VWQueue;
 import filenet.vw.api.VWSession;
 import filenet.vw.api.VWWorkBasket;
+import filenet.vw.api.VWWorkObject;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 /**
  * Created by rmugattarov on 01.02.2016.
@@ -18,7 +22,14 @@ public class RetrieveWorkflow {
             vwSession.logon("cpe_bootstrap@tn.fntst.ru", "o9p0[-]=", "CP01");
             VWQueue docQueue = vwSession.getQueue("DocQueue");
             VWWorkBasket basket = docQueue.fetchWorkBasket("Связывание с Заголовком дела");
-            System.out.println(basket.getName());
+            int filteredCount = basket.fetchFilteredCount(0, null, null, null, null, null, 1);
+            System.out.println(filteredCount);
+            VWWorkBasket.QueryResults queryResults = basket.fetchFilteredBatch(0, filteredCount, null, null, null, null, null, null, 1);
+            VWWorkObject[] workObjects = (VWWorkObject[]) queryResults.getWorkObjects();
+            int i = 0;
+            for (VWWorkObject workObject : workObjects) {
+                System.out.printf("%d) %s\n", ++i, workObject.getFieldValue("F_Subject"));
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
