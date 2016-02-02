12f70ceb636766d954e29a29905353c119fe661a
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-02-02 14:12:20 +0500

    updates

--- src/test/java/rmugattarov/filenet/pe/RetrieveWorkflow.java
+++ src/test/java/rmugattarov/filenet/pe/RetrieveWorkflow.java
@@ -1,13 +1,10 @@
 package rmugattarov.filenet.pe;
 
-import com.filenet.api.constants.PropertyNames;
 import filenet.vw.api.VWQueue;
 import filenet.vw.api.VWSession;
 import filenet.vw.api.VWWorkBasket;
 import filenet.vw.api.VWWorkObject;
 import org.junit.Test;
-
-import java.util.Arrays;
 
 /**
  * Created by rmugattarov on 01.02.2016.
@@ -22,9 +19,8 @@ public class RetrieveWorkflow {
             vwSession.logon("cpe_bootstrap@tn.fntst.ru", "o9p0[-]=", "CP01");
             VWQueue docQueue = vwSession.getQueue("DocQueue");
             VWWorkBasket basket = docQueue.fetchWorkBasket("Связывание с Заголовком дела");
-            int filteredCount = basket.fetchFilteredCount(0, null, null, null, null, null, 1);
-            System.out.println(filteredCount);
-            VWWorkBasket.QueryResults queryResults = basket.fetchFilteredBatch(0, filteredCount, null, null, null, null, null, null, 1);
+            VWWorkBasket.QueryResults queryResults = basket.fetchFilteredBatch(0, 1, null, null, null, "F_Subject LIKE :F_Subject", new String[]{"%1200837289008%"}, null, 1);
+            System.out.println(queryResults);
             VWWorkObject[] workObjects = (VWWorkObject[]) queryResults.getWorkObjects();
             int i = 0;
             for (VWWorkObject workObject : workObjects) {
