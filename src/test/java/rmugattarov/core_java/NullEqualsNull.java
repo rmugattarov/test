ca645cbfb0714f43b905f98b751af090646c220f
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-02-04 16:15:45 +0500

    updates

---
+++ src/test/java/rmugattarov/core_java/NullEqualsNull.java
@@ -0,0 +1,13 @@
+package rmugattarov.core_java;
+
+import org.junit.Test;
+
+/**
+ * Created by rmugattarov on 04.02.2016.
+ */
+public class NullEqualsNull {
+    @Test
+    public void test() {
+        System.out.println(null == null);
+    }
+}
