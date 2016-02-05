622dc5f76593803c8ccdcc8e8dfc281e26f890fc
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-02-05 16:01:30 +0500

    updates

--- src/test/java/rmugattarov/lecm/TNFDEV_9247_construction_objects_optmn/TNFDEV_9247.java
+++ src/test/java/rmugattarov/lecm/TNFDEV_9247_construction_objects_optmn/TNFDEV_9247.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by rmugattarov on 05.02.2016.
@@ -15,10 +16,11 @@ public class TNFDEV_9247 {
     @Test
     public void test() throws IOException {
         NtlmPasswordAuthentication auth = new NtlmPasswordAuthentication("tn.fntst.ru", "cpe_bootstrap@tn.fntst.ru", "o9p0[-]=");
-        BufferedInputStream inputStream = new BufferedInputStream(new SmbFileInputStream(new SmbFile("smb://172.28.24.185/tstxml/KatStroy.XML", auth)));
-        int c;
-        while ((c = inputStream.read()) != -1) {
-            System.out.print((char) c);
+        try (InputStreamReader inputStream = new InputStreamReader(new BufferedInputStream(new SmbFileInputStream(new SmbFile("smb://172.28.24.185/tstxml/KatStroy.XML", auth))), "UTF-8")) {
+            int c;
+            while ((c = inputStream.read()) != -1) {
+                System.out.print((char) c);
+            }
         }
     }
 }
