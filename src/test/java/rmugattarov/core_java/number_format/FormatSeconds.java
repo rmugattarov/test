d8200f637a21280e0353268e3bbf91347ce14d18
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-03-03 16:17:09 +0500

    updates

---
+++ src/test/java/rmugattarov/core_java/number_format/FormatSeconds.java
@@ -0,0 +1,13 @@
+package rmugattarov.core_java.number_format;
+
+import org.junit.Test;
+
+/**
+ * Created by rmugattarov on 03.03.2016.
+ */
+public class FormatSeconds {
+    @Test
+    public void test() {
+        System.out.println(123/1000D);
+    }
+}
