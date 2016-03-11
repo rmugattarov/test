cccf17c546cc44712c1363dbd3c15ef11afb9b33
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-03-11 19:11:16 +0500

    updates

---
+++ src/test/java/rmugattarov/core_java/strings/StartsWith.java
@@ -0,0 +1,15 @@
+package rmugattarov.core_java.strings;
+
+import org.junit.Test;
+
+/**
+ * Created by rmugattarov on 11.03.2016.
+ */
+public class StartsWith {
+    @Test
+    public void test() {
+        String s1 = "abc";
+        String s2 = "abc";
+        System.out.println(s1.startsWith(s2));
+    }
+}
