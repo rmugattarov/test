ae1a99d4701a48eb1c2bbaaa63c6faa484a0a5d0
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-03-31 10:17:46 +0500

    updates

---
+++ src/test/java/rmugattarov/core_java/NullInstanceof.java
@@ -0,0 +1,15 @@
+package rmugattarov.core_java;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+/**
+ * Created by rmugattarov on 31.03.2016.
+ */
+public class NullInstanceof {
+    @Test
+    public void test() {
+        System.out.println(null instanceof Date);
+    }
+}
