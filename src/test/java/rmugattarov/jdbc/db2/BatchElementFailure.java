54ade0ced5f6e6257de34d9663a1b780d74bf72e
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-04-04 11:26:05 +0500

    updates

--- src/test/java/rmugattarov/jdbc/db2/BatchElementFailure.java
+++ src/test/java/rmugattarov/jdbc/db2/BatchElementFailure.java
@@ -1,11 +1,12 @@
 package rmugattarov.jdbc.db2;
 
+import com.ibm.db2.jcc.am.SqlIntegrityConstraintViolationException;
 import org.junit.Test;
 
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Random;
 
 /**
  * Created by rmugattarov on 31.03.2016.
@@ -15,19 +16,36 @@ public class BatchElementFailure {
     private String dbms = "db2";
     private String port = "50000";
     private String db = "REFDB182";
+    private Random rand = new Random();
 
     @Test
     public void test() throws SQLException {
         java.util.Properties connProperties = new java.util.Properties();
         connProperties.put("user", "refuser");
         connProperties.put("password", "o9p0[-]=");
-        int batchSize = 10;
-        try (java.sql.Connection connection = DriverManager.getConnection("jdbc:" + dbms + "://" + host + ":" + port + "/" + db, connProperties); Statement stmt = connection.createStatement()) {
-            ResultSet idSet = stmt.executeQuery("SELECT login FROM login_audit FETCH FIRST " + batchSize + " ROWS ONLY");
-            int counter = 0;
-            while (idSet.next()) {
-                System.out.printf("%d) %s\n", ++counter, idSet.getString(1));
-            }
+        try (java.sql.Connection connection = DriverManager.getConnection("jdbc:" + dbms + "://" + host + ":" + port + "/" + db, connProperties); PreparedStatement stmt = connection.prepareStatement(
+                "INSERT INTO login_audit(login,login_time,last_action_time, result,ip) VALUES(?,?,?,?,?)"
+        )) {
+            setPsParams(stmt, "dummy_login", new Timestamp(0L), new Timestamp(0L), 0, "dummy_ip");
+            stmt.addBatch();
+            setPsParams(stmt, "dummy_" + Math.random(), new Timestamp(new Date().getTime()), new Timestamp(new Date().getTime()), rand.nextInt(), "dummy_" + Math.random() + "");
+            stmt.addBatch();
+            setPsParams(stmt, "dummy_login", new Timestamp(0L), new Timestamp(0L), 0, "dummy_ip");
+            stmt.addBatch();
+            setPsParams(stmt, "dummy_" + Math.random(), new Timestamp(new Date().getTime()), new Timestamp(new Date().getTime()), rand.nextInt(), "dummy_" + Math.random() + "");
+            stmt.addBatch();
+            setPsParams(stmt, "dummy_" + Math.random(), new Timestamp(new Date().getTime()), new Timestamp(new Date().getTime()), rand.nextInt(), "dummy_" + Math.random() + "");
+            stmt.addBatch();
+            System.out.println(Arrays.toString(stmt.executeBatch()));
+            System.out.println("DONE");
         }
+    }
+
+    private void setPsParams(PreparedStatement stmt, String login, Timestamp login_time, Timestamp lastActionTime, Integer result, String ip) throws SQLException {
+        stmt.setString(1, login);
+        stmt.setTimestamp(2, login_time);
+        stmt.setTimestamp(3, lastActionTime);
+        stmt.setInt(4, result);
+        stmt.setString(5, ip);
     }
 }
