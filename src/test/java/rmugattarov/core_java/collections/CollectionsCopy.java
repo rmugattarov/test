a428d9ae05a13bb10547f07fe14339b2abba1a58
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-04-04 14:05:20 +0500

    updates

---
+++ src/test/java/rmugattarov/core_java/collections/CollectionsCopy.java
@@ -0,0 +1,12 @@
+package rmugattarov.core_java.collections;
+
+import org.junit.Test;
+
+/**
+ * Created by rmugattarov on 04.04.2016.
+ */
+public class CollectionsCopy {
+    @Test
+    public void test() {
+    }
+}
