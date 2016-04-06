c35c11ad8d430b85c6fe4612abaea9997246bdc4
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-04-06 17:36:36 +0500

    updates

---
+++ src/test/java/rmugattarov/core_java/collections/TestHashMap.java
@@ -0,0 +1,16 @@
+package rmugattarov.core_java.collections;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+
+/**
+ * Created by rmugattarov on 06.04.2016.
+ */
+public class TestHashMap {
+    @Test
+    public void test() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("k1", "v1");
+    }
+}
