c417f42f49eb85459c53cd051aed7f4d25cba88a
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-04-07 18:01:13 +0500

    updates

--- src/test/java/rmugattarov/core_java/exceptions/TestSupressed.java
+++ src/test/java/rmugattarov/core_java/exceptions/TestSupressed.java
@@ -2,6 +2,8 @@ package rmugattarov.core_java.exceptions;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
  * Created by rmugattarov on 07.04.2016.
  */
@@ -9,9 +11,14 @@ public class TestSupressed {
     @Test
     public void test() {
         try {
-            throw new RuntimeException("One");
-        } finally {
-            throw new RuntimeException("Two");
+            try {
+                throw new RuntimeException("One");
+            } finally {
+                throw new RuntimeException("Two");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println(Arrays.toString(e.getSuppressed()));
         }
     }
 }
