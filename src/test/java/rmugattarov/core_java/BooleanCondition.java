1c837081e26bd650c46134ae68835371ad199dbe
rmugattarov <mugattarov@gmail.com>
2016-04-12 19:58:45 +0500

    updates

---
+++ src/test/java/rmugattarov/core_java/BooleanCondition.java
@@ -0,0 +1,15 @@
+package rmugattarov.core_java;
+
+import org.junit.Test;
+
+/**
+ * Created by rmugattarov on 26.03.2016.
+ */
+public class BooleanCondition {
+    @Test
+    public void test() {
+        int y = 0;
+        int x = 0;
+        boolean result = (y == 0) & (x == 0);
+    }
+}
