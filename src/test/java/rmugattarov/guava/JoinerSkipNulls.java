fb107c4b5a752a09653c5bca0927e3bda311ddd3
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-05-05 19:03:26 +0500

    updates

--- src/test/java/rmugattarov/guava/JoinerSkipNulls.java
+++ src/test/java/rmugattarov/guava/JoinerSkipNulls.java
@@ -3,12 +3,14 @@ package rmugattarov.guava;
 import com.google.common.base.Joiner;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
  * Created by rmugattarov on 02.03.2016.
  */
 public class JoinerSkipNulls {
     @Test
     public void test() {
-        System.out.println(Joiner.on(" ").skipNulls().join("one", null, "three ").trim());
+        System.out.println(Joiner.on(" ").skipNulls().join(Arrays.asList("one", "two", null)).trim());
     }
 }
