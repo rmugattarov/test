dca75aac95cf622a4652b71a8d82b6aa0a8c05f4
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-05-10 10:53:15 +0500

    updates

---
+++ src/test/java/rmugattarov/core_java/dates/GetYear.java
@@ -0,0 +1,15 @@
+package rmugattarov.core_java.dates;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+/**
+ * Created by rmugattarov on 10.05.2016.
+ */
+public class GetYear {
+    @Test
+    public void test() {
+        System.out.println(new Date().getYear());
+    }
+}
