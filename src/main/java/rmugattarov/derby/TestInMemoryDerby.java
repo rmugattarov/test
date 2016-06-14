7b4833b0772e4d80fd1b9aa266a8e95a04f20490
rmugattarov <mugattarov@gmail.com>
2016-06-14 13:35:20 +0500

    updates

---
+++ src/main/java/rmugattarov/derby/TestInMemoryDerby.java
@@ -0,0 +1,20 @@
+package rmugattarov.derby;
+
+import java.sql.*;
+
+/**
+ * Created by rmugattarov on 14.06.2016.
+ */
+public class TestInMemoryDerby {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
+        Connection connection = DriverManager.getConnection("jdbc:derby:memory:myDB;create=true");
+        Statement statement = connection.createStatement();
+        statement.executeUpdate("CREATE TABLE TEST_TABLE (ID BIGINT GENERATED ALWAYS AS IDENTITY, TEXT VARCHAR(32))");
+        statement.executeUpdate("INSERT INTO TEST_TABLE (TEXT) VALUES ('hello')");
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM TEST_TABLE");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString(2));
+        }
+    }
+}
