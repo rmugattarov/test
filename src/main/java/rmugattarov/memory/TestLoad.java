3df74e6282eda5c3cb45c8e3b1d9e6861607e141
rmugattarov <mugattarov@gmail.com>
2016-06-14 19:03:51 +0500

    updates

--- src/main/java/rmugattarov/memory/TestLoad.java
+++ src/main/java/rmugattarov/memory/TestLoad.java
@@ -7,6 +7,9 @@ public class TestLoad {
     public static void main(String[] args) {
         String s = "";
         for (int i = 0; i < 1000000; i++) {
+            System.out.println("MaxM : " + Runtime.getRuntime().maxMemory());
+            System.out.println("FreeM : " + Runtime.getRuntime().freeMemory());
+            System.out.println("Threads : " + Thread.activeCount());
             s += i;
         }
     }
