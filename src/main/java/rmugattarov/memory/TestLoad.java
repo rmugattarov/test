588988bec83b2c319fad66c65e9cfa0052ecbc06
rmugattarov <mugattarov@gmail.com>
2016-06-14 14:49:24 +0500

    updates

---
+++ src/main/java/rmugattarov/memory/TestLoad.java
@@ -0,0 +1,13 @@
+package rmugattarov.memory;
+
+/**
+ * Created by rmugattarov on 14.06.2016.
+ */
+public class TestLoad {
+    public static void main(String[] args) {
+        String s = "";
+        for (int i = 0; i < 1000000; i++) {
+            s += i;
+        }
+    }
+}
