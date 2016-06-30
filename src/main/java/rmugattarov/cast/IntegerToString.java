a7153b11afaf9d519d37a4080717889c93b1bb67
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-06-30 14:37:01 +0500

    updates

---
+++ src/main/java/rmugattarov/cast/IntegerToString.java
@@ -0,0 +1,12 @@
+package rmugattarov.cast;
+
+/**
+ * Created by rmugattarov on 30.06.2016.
+ */
+public class IntegerToString {
+    public static void main(String[] args) {
+        Object o = new Integer(1);
+        String s  = (String) o;
+        System.out.println(s);
+    }
+}
