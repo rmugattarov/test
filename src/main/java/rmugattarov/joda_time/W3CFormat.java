5789316cb795da74e6917be0b7af8af7799bad00
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-07-08 14:20:28 +0500

    updates

---
+++ src/main/java/rmugattarov/joda_time/W3CFormat.java
@@ -0,0 +1,16 @@
+package rmugattarov.joda_time;
+
+import org.joda.time.DateTimeZone;
+import org.joda.time.LocalDateTime;
+
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by rmugattarov on 08.07.2016.
+ */
+public class W3CFormat {
+    public static void main(String[] args) {
+        System.out.println(new LocalDateTime(new Date()).toDate(TimeZone.getTimeZone("UTC")));
+    }
+}
