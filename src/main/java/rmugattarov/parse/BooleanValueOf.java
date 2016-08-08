49c4d70d9c779b095c26fb6558603b2d8588bdb4
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-08-08 17:25:17 +0500

    updates

---
+++ src/main/java/rmugattarov/parse/BooleanValueOf.java
@@ -0,0 +1,10 @@
+package rmugattarov.parse;
+
+/**
+ * Created by rmugattarov on 08.08.2016.
+ */
+public class BooleanValueOf {
+    public static void main(String[] args) {
+        System.out.println(Boolean.valueOf(null));
+    }
+}
