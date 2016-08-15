264ef33e13dd61dc0a9c1e0731c4e4e64f30f321
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-08-15 15:57:20 +0500

    updates

---
+++ src/main/java/rmugattarov/collections/LinkedVsArray.java
@@ -0,0 +1,35 @@
+package rmugattarov.collections;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by rmugattarov on 15.08.2016.
+ */
+public class LinkedVsArray {
+    public static void main(String[] args) {
+        Date date1 = new Date();
+        List<Long> arrayList = new ArrayList<>();
+        for (long i = 0; i < 10000000L; i++) {
+            arrayList.add(i);
+        }
+        for (Long integer : arrayList) {
+
+        }
+        Date date2 = new Date();
+        System.out.printf("ArrayList : %d ms\r\n", (date2.getTime() - date1.getTime()));
+
+        Date date3 = new Date();
+        List<Long> linkedList = new LinkedList<>();
+        for (long i = 0; i < 10000000L; i++) {
+            linkedList.add(i);
+        }
+        for (Long integer : linkedList) {
+
+        }
+        Date date4 = new Date();
+        System.out.printf("LinkedList : %d ms\r\n", (date4.getTime() - date3.getTime()));
+    }
+}
