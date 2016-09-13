d791a6bc25914c871daaeedba475a99e76f66b4a
rmugattarov <mugattarov@gmail.com>
2016-09-13 15:10:04 +0300

    updates

---
+++ src/main/java/rmugattarov/interfaces/ObjectMethods.java
@@ -0,0 +1,8 @@
+package rmugattarov.interfaces;
+
+/**
+ * Created by rmugattarov on 13.09.2016.
+ */
+public interface ObjectMethods {
+    boolean equals(Object o);
+}
---
+++ src/main/java/rmugattarov/interfaces/ObjectMethodsImpl.java
@@ -0,0 +1,7 @@
+package rmugattarov.interfaces;
+
+/**
+ * Created by rmugattarov on 13.09.2016.
+ */
+public class ObjectMethodsImpl implements ObjectMethods {
+}
