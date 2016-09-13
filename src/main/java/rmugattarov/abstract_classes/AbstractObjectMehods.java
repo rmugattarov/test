0bdaa6d008ab0138f6ee42df5c95e0bbbee146ae
rmugattarov <mugattarov@gmail.com>
2016-09-13 15:11:55 +0300

    updates

---
+++ src/main/java/rmugattarov/abstract_classes/AbstractObjectMehods.java
@@ -0,0 +1,8 @@
+package rmugattarov.abstract_classes;
+
+/**
+ * Created by rmugattarov on 13.09.2016.
+ */
+public abstract class AbstractObjectMehods {
+    public abstract boolean equals(Object o);
+}
---
+++ src/main/java/rmugattarov/abstract_classes/AbstractObjectMehodsImpl.java
@@ -0,0 +1,11 @@
+package rmugattarov.abstract_classes;
+
+/**
+ * Created by rmugattarov on 13.09.2016.
+ */
+public class AbstractObjectMehodsImpl extends AbstractObjectMehods {
+    @Override
+    public boolean equals(Object o) {
+        return false;
+    }
+}
