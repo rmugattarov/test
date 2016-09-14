a33deff5054e70493edcf733ec9c4124dae53b86
rmugattarov <mugattarov@gmail.com>
2016-09-14 10:42:50 +0300

    updates

---
+++ src/main/java/rmugattarov/hashcode/ClassHash.java
@@ -0,0 +1,10 @@
+package rmugattarov.hashcode;
+
+/**
+ * Created by rmugattarov on 14.09.2016.
+ */
+public class ClassHash {
+    public static void main(String[] args) {
+        System.out.println(String.class.hashCode());
+    }
+}
