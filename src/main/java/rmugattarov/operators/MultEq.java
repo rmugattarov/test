8685c4c55a3ffad6b4944c0ecd1798b0eb0304c6
rmugattarov <mugattarov@gmail.com>
2016-09-15 10:48:57 +0300

    updates

---
+++ src/main/java/rmugattarov/operators/MultEq.java
@@ -0,0 +1,13 @@
+package rmugattarov.operators;
+
+/**
+ * Created by rmugattarov on 15.09.2016.
+ */
+public class MultEq {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        b *= a * 4;
+        System.out.println(b);
+    }
+}
