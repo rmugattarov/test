5298e3be6b6f83b517e08be4758aed11630d8aca
rmugattarov <mugattarov@gmail.com>
2016-09-16 09:08:29 +0300

    updates

---
+++ src/main/java/rmugattarov/hashcode/MutableHash.java
@@ -0,0 +1,55 @@
+package rmugattarov.hashcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by rmugattarov on 15.09.2016.
+ */
+public class MutableHash {
+    private String s;
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = s == null ? 1 : s.hashCode();
+        System.out.println("hashCode called. value : " + hash);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        boolean result;
+        if (o == null) {
+            result = false;
+        } else if (o == this) {
+            result = true;
+        } else if (getClass() != o.getClass()) {
+            result = false;
+        } else {
+            MutableHash that = (MutableHash) o;
+            result = this.s.equals(that.s);
+        }
+        System.out.println("equals called. result " + result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Set<MutableHash> set = new HashSet<>();
+        MutableHash o1 = new MutableHash();
+        set.add(o1);
+        System.out.println("size : " + set.size());
+        set.add(o1);
+        System.out.println("size : " + set.size());
+        o1.setS("123");
+        set.add(o1);
+        System.out.println("size : " + set.size());
+    }
+}
