db4984aba777ec9cbdc14e352433ce87dc4dc326
rmugattarov <mugattarov@gmail.com>
2016-09-19 16:47:48 +0300

    updates

---
+++ src/main/java/rmugattarov/equals/EmptyObjects.java
@@ -0,0 +1,11 @@
+package rmugattarov.equals;
+
+/**
+ * Created by rmugattarov on 19.09.2016.
+ */
+public class EmptyObjects {
+    public static void main(String[] args) {
+        System.out.println(new Object().hashCode());
+        System.out.println(new Object().hashCode());
+    }
+}
