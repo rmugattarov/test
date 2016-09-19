4fd03a9529e9e06a7a9d40c23d9a9580af82b1cb
rmugattarov <mugattarov@gmail.com>
2016-09-19 16:48:33 +0300

    updates

--- src/main/java/rmugattarov/equals/EmptyObjects.java
+++ src/main/java/rmugattarov/equals/EmptyObjects.java
@@ -7,5 +7,8 @@ public class EmptyObjects {
     public static void main(String[] args) {
         System.out.println(new Object().hashCode());
         System.out.println(new Object().hashCode());
+        System.out.println(new Object().hashCode());
+        System.out.println(new Object().hashCode());
+        System.out.println(new Object().hashCode());
     }
 }
