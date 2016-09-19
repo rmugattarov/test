93a7341df32667bd43b361becedc54f2cf424612
rmugattarov <mugattarov@gmail.com>
2016-09-19 12:19:47 +0300

    updates

---
+++ src/main/java/rmugattarov/memory/ObjectSize.java
@@ -0,0 +1,28 @@
+package rmugattarov.memory;
+
+import jdk.nashorn.internal.ir.debug.ObjectSizeCalculator;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Created by rmugattarov on 19.09.2016.
+ */
+public class ObjectSize {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        LinkedList<String> linkedList = new LinkedList<>();
+
+        System.out.printf("Empty ArrayList : %d\r\n", ObjectSizeCalculator.getObjectSize(arrayList));
+        System.out.printf("Empty LinkedList : %d\r\n", ObjectSizeCalculator.getObjectSize(linkedList));
+
+        int elementCount = 3_000_000;
+        for (int i = 0; i < elementCount; i++) {
+            arrayList.add(null);
+            linkedList.add(null);
+        }
+
+        System.out.printf("1M ArrayList : %d KB per element\r\n", ObjectSizeCalculator.getObjectSize(arrayList) / elementCount);
+        System.out.printf("1M LinkedList : %d KB per element\r\n", ObjectSizeCalculator.getObjectSize(linkedList) / elementCount);
+    }
+}
