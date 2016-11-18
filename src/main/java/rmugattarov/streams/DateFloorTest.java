217dbeb1f061fac1777a11e9f76f2376b2ddec05
rmugattarov <mugattarov@gmail.com>
2016-11-18 17:36:09 +0300

    updates

---
+++ src/main/java/rmugattarov/streams/DateFloorTest.java
@@ -0,0 +1,18 @@
+package rmugattarov.streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by rmugattarov on 17.11.2016.
+ */
+public class DateFloorTest {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("aaa", "bbb", "ccc");
+        System.out.println(floor(list, "a"));
+    }
+
+    public static String floor(List<String> events, String ceiling) {
+        return events.stream().filter(e -> e.compareTo(ceiling) <= 0).reduce((a, b) -> b).orElse(null);
+    }
+}
