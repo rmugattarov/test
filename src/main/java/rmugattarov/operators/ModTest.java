a95ee3607984f01ca6ec54988abd77e3d1f560dc
rmugattarov <mugattarov@gmail.com>
2016-11-23 12:25:08 +0300

    updates

---
+++ src/main/java/rmugattarov/operators/ModTest.java
@@ -0,0 +1,15 @@
+package rmugattarov.operators;
+
+/**
+ * Created by rmugattarov on 23.11.2016.
+ */
+public class ModTest {
+    public static void main(String[] args) {
+        System.out.println(0 % 2);
+        System.out.println(1 % 2);
+        System.out.println(2 % 2);
+        System.out.println(3 % 2);
+        System.out.println(4 % 2);
+        System.out.println(5 % 2);
+    }
+}
