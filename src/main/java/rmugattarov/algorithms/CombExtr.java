9a9ac544d29f4a9ba47eef0ebfb4b73c20134ecf
rmugattarov <mugattarov@gmail.com>
2016-12-08 10:34:53 +0300

    updates

--- src/main/java/rmugattarov/algorithms/CombExtr.java
+++ src/main/java/rmugattarov/algorithms/CombExtr.java
@@ -1,9 +1,12 @@
 package rmugattarov.algorithms;
 
+import java.util.Arrays;
+import java.util.Iterator;
+
 /**
  * Created by rmugattarov on 08.12.2016.
  */
-public class CombExtr {
+public class CombExtr implements Iterator<Object[]> {
 
     private final Object[] data;
     private final int[] idx;
@@ -19,13 +22,25 @@ public class CombExtr {
         }
     }
 
+    @Override
     public boolean hasNext() {
         return idx[0] != data.length - idx.length;
+    }
+
+    @Override
+    public Object[] next() {
+        Object[] result = new Object[idx.length];
+        for (int i = 0; i < idx.length; i++) {
+            result[i] = data[idx[i]];
+        }
+        return result;
     }
 
 
     public static void main(String[] args) {
         CombExtr extr = new CombExtr(new Integer[]{1, 2, 3}, 2);
-        System.out.println(extr.hasNext());
+        if (extr.hasNext()) {
+            System.out.println(Arrays.toString(extr.next()));
+        }
     }
 }
