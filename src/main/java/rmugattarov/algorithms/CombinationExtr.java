c201a911b94c4e170f138db5665cab9ae5e4bfa9
rmugattarov <mugattarov@gmail.com>
2016-12-08 19:05:38 +0300

    updates

--- src/main/java/rmugattarov/algorithms/CombinationExtr.java
+++ src/main/java/rmugattarov/algorithms/CombinationExtr.java
@@ -10,33 +10,27 @@ public class CombinationExtr {
         int len = 5;
         int[] idx = new int[]{0, 1, 2};
         System.out.println(Arrays.toString(idx));
-        for (int i = idx.length - 1; i >= 0; i--) {
-            if (idx[0] == len - idx.length) {
-                break;
-            }
-            if (i == 0) {
-                for (int j = 0; j < idx.length; j++) {
-                    if (j == 0) {
-                        idx[j] = idx[j] + 1;
-                    } else {
-                        idx[j] = idx[j - 1] + 1;
-                    }
-                }
-                i = idx.length - 1;
+        int last = idx.length - 1;
+        while (idx[0] < len - idx.length) {
+            idx[last] = idx[last] + 1;
+            if (idx[last] > len) {
+                raise(idx, last - 1, len);
                 System.out.println(Arrays.toString(idx));
             } else {
-                if (idx[i] + 1 > len) {
-                    idx[i - 1] = idx[i - 1] + 1;
-                    for (int j = i; j < idx.length; j++) {
-                        idx[j] = idx[j - 1] + 1;
-                    }
-                    i = idx.length - 1;
-                    System.out.println(Arrays.toString(idx));
-                } else {
-                    idx[i] = idx[i] + 1;
-                    System.out.println(Arrays.toString(idx));
-                }
+                System.out.println(Arrays.toString(idx));
             }
         }
     }
+
+    private static void raise(int[] idx, int i, int len) {
+        idx[i] = idx[i] + 1;
+        if (idx[i] > len) {
+            raise(idx, i - 1, len);
+        } else {
+            for (int j = i + 1; j < idx.length; j++) {
+                idx[j] = idx[j - 1] + 1;
+            }
+        }
+    }
+
 }
