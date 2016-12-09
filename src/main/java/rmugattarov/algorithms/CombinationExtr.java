993731542a18306cfe10a408c4e80e5a6cd82396
rmugattarov <mugattarov@gmail.com>
2016-12-09 10:02:27 +0300

    updates

--- src/main/java/rmugattarov/algorithms/CombinationExtr.java
+++ src/main/java/rmugattarov/algorithms/CombinationExtr.java
@@ -1,31 +1,37 @@
 package rmugattarov.algorithms;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
 /**
  * Created by rmugattarov on 08.12.2016.
  */
-public class CombinationExtr {
+public class CombinationExtr implements Iterator<Object[]> {
+
+    private final Object[] data;
+    private final int[] idx;
+    private final int lastIdx;
+
+    public CombinationExtr(Object[] data, int len) {
+        this.data = data;
+        idx = new int[len];
+        for (int i = 0; i < idx.length; i++) {
+            idx[i] = i;
+        }
+        lastIdx = idx.length - 1;
+    }
+
     public static void main(String[] args) {
-        int len = 5;
-        int[] idx = new int[]{0, 1, 2};
-        System.out.printf("initial : %s\r\n", Arrays.toString(idx));
-        int last = idx.length - 1;
-        while (idx[0] <= len - idx.length) {
-            idx[last] = idx[last] + 1;
-            if (idx[last] > len) {
-                raise(idx, last - 1, len);
-                System.out.printf("raised : %s\r\n", Arrays.toString(idx));
-            } else {
-                System.out.printf("grown : %s\r\n", Arrays.toString(idx));
-            }
+        CombinationExtr extr = new CombinationExtr(new Integer[]{1, 2, 3, 4, 5}, 3);
+        while (extr.hasNext()) {
+            System.out.println(Arrays.toString(extr.next()));
         }
     }
 
-    private static void raise(int[] idx, int i, int len) {
+    private void raise(int[] idx, int i) {
         idx[i] = idx[i] + 1;
-        if (idx[i] + i > len) {
-            raise(idx, i - 1, len);
+        if (idx[i] + i >= data.length) {
+            raise(idx, i - 1);
         } else {
             for (int j = i + 1; j < idx.length; j++) {
                 idx[j] = idx[j - 1] + 1;
@@ -33,4 +39,29 @@ public class CombinationExtr {
         }
     }
 
+    @Override
+    public boolean hasNext() {
+        return idx[0] < data.length - idx.length;
+    }
+
+    @Override
+    public Object[] next() {
+        Object[] result;
+        if (idx[lastIdx] >= data.length) {
+            raise(idx, lastIdx - 1);
+            result = collect(idx);
+        } else {
+            result = collect(idx);
+        }
+        idx[lastIdx] = idx[lastIdx] + 1;
+        return result;
+    }
+
+    private Object[] collect(int[] idx) {
+        Object[] result = new Object[idx.length];
+        for (int i = 0; i < idx.length; i++) {
+            result[i] = data[idx[i]];
+        }
+        return result;
+    }
 }
