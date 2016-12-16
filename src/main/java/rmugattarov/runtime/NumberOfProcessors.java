3d8614d4d31d5244e5eaf72abdc2746ac34da23e
rmugattarov <mugattarov@gmail.com>
2016-12-16 15:14:49 +0300

    updates

---
+++ src/main/java/rmugattarov/runtime/NumberOfProcessors.java
@@ -0,0 +1,10 @@
+package rmugattarov.runtime;
+
+/**
+ * Created by rmugattarov on 16.12.2016.
+ */
+public class NumberOfProcessors {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
