cc2e83c71c4fbcb85486055abe6166881e4da009
rmugattarov <mugattarov@gmail.com>
2016-12-20 14:22:59 +0300

    updates

--- src/main/java/rmugattarov/algorithms/DenormPerm.java
+++ src/main/java/rmugattarov/algorithms/DenormPerm.java
@@ -7,4 +7,18 @@ public class DenormPerm {
     public static void main(String[] args) {
 
     }
+
+    private static void denorm(int[] idx, int denormVal) {
+        int mid = idx.length / 2;
+        int countdouwn = denormVal;
+        int pendulum = 0;
+        while (countdouwn > 0) {
+            int pivot = mid + pendulum;
+            if (pivot < 0 || pivot >= idx.length) {
+                pendulum = 0;
+                pivot = mid;
+            }
+
+        }
+    }
 }
