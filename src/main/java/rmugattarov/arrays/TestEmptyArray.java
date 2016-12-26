ac028a0e653b2f5a8af9d7e85d312e9e6887f9a2
rmugattarov <mugattarov@gmail.com>
2016-12-26 11:24:50 +0300

    updates

---
+++ src/main/java/rmugattarov/arrays/TestEmptyArray.java
@@ -0,0 +1,12 @@
+package rmugattarov.arrays;
+
+/**
+ * Created by rmugattarov on 26.12.2016.
+ */
+public class TestEmptyArray {
+    public static void main(String[] args) {
+        int[] arr = new int[2];
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+    }
+}
