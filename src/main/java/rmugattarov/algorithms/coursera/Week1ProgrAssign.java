b9e9d663daae31f1233e44ee14e6d51cd8842c25
rmugattarov <mugattarov@gmail.com>
2017-01-13 18:24:19 +0300

    updates

---
+++ src/main/java/rmugattarov/algorithms/coursera/Week1ProgrAssign.java
@@ -0,0 +1,16 @@
+package rmugattarov.algorithms.coursera;
+
+/**
+ * Created by rmugattarov on 13.01.2017.
+ */
+public class Week1ProgrAssign {
+    public static void main(String[] args) {
+//        System.out.println(multiply("2", "4"));
+//        System.out.println(multiply("3", "7"));
+//        System.out.println(multiply("11", "11"));
+    }
+
+//    private static String multiply(String left, String right) {
+//
+//    }
+}
