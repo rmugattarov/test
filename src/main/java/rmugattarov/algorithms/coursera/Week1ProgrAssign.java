00ef70cf37c00decdcd642778cb996b2bd49f27a
rmugattarov <mugattarov@gmail.com>
2017-01-15 13:46:10 +0300

    updates

--- src/main/java/rmugattarov/algorithms/coursera/Week1ProgrAssign.java
+++ src/main/java/rmugattarov/algorithms/coursera/Week1ProgrAssign.java
@@ -1,16 +1,12 @@
 package rmugattarov.algorithms.coursera;
 
+import java.math.BigInteger;
+
 /**
  * Created by rmugattarov on 13.01.2017.
  */
 public class Week1ProgrAssign {
     public static void main(String[] args) {
-//        System.out.println(multiply("2", "4"));
-//        System.out.println(multiply("3", "7"));
-//        System.out.println(multiply("11", "11"));
+        System.out.println(new BigInteger("3141592653589793238462643383279502884197169399375105820974944592").multiply(new BigInteger("2718281828459045235360287471352662497757247093699959574966967627")));
     }
-
-//    private static String multiply(String left, String right) {
-//
-//    }
 }
