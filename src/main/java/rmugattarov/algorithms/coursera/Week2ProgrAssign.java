547ce9c1ec7de921ad375a0016b8005e07dd3c2d
rmugattarov <mugattarov@gmail.com>
2017-01-17 23:25:07 +0300

    updates

--- src/main/java/rmugattarov/algorithms/coursera/Week2ProgrAssign.java
+++ src/main/java/rmugattarov/algorithms/coursera/Week2ProgrAssign.java
@@ -1,10 +1,21 @@
 package rmugattarov.algorithms.coursera;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 /**
  * Created by rmugattarov on 17.01.2017.
  */
 public class Week2ProgrAssign {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        List<Integer> list = new ArrayList<>(100000);
+        try (Stream<String> lines = Files.lines(Paths.get("src/main/java/rmugattarov/algorithms/coursera/IntegerArray.txt"))) {
+            lines.forEach(s -> list.add(Integer.valueOf(s)));
+        }
+        System.out.println(list.size());
     }
 }
