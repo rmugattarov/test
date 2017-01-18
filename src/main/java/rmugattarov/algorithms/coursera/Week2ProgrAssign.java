d688663f03b8ee966ea79fb69afc467b55f538ff
rmugattarov <mugattarov@gmail.com>
2017-01-18 09:57:27 +0300

    updates

--- src/main/java/rmugattarov/algorithms/coursera/Week2ProgrAssign.java
+++ src/main/java/rmugattarov/algorithms/coursera/Week2ProgrAssign.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -11,24 +12,26 @@ import java.util.stream.Stream;
  * Created by rmugattarov on 17.01.2017.
  */
 public class Week2ProgrAssign {
-    private static int count = 0;
+
     public static void main(String[] args) throws IOException {
         List<Integer> list = new ArrayList<>(100000);
         try (Stream<String> lines = Files.lines(Paths.get("src/main/java/rmugattarov/algorithms/coursera/IntegerArray.txt"))) {
             lines.forEach(s -> list.add(Integer.valueOf(s)));
         }
-        System.out.println(count(list));
-        System.out.println(count);
+        System.out.println(countInversionsLoop(list));
     }
 
-    private static int count(List<Integer> list) {
-        if (list.size() == 0) {
-            return 0;
-        } else if (list.size() == 1) {
-            count++;
-            return 1;
-        } else {
-            return count(list.subList(0, list.size() / 2)) + count(list.subList(list.size() / 2, list.size()));
+    private static long countInversionsLoop(List<Integer> list) {
+        long count = 0;
+        for (int i = 0; i < list.size() - 1; i++) {
+            Integer left = list.get(i);
+            for (int j = i + 1; j < list.size(); j++) {
+                Integer right = list.get(j);
+                if (left > right) {
+                    count++;
+                }
+            }
         }
+        return count;
     }
 }
