944bb7819a2922ce78ec3971b44a1b17a6c38311
rmugattarov <mugattarov@gmail.com>
2017-04-04 11:04:38 +0300

    updates

---
+++ src/main/java/rmugattarov/cast/CastToBoolean.java
@@ -0,0 +1,10 @@
+package rmugattarov.cast;
+
+/**
+ * Created by rmugattarov on 04.04.2017.
+ */
+public class CastToBoolean {
+    public static void main(String[] args) {
+        Boolean b = (Boolean) new Object();
+    }
+}
