a13d40954abfcc1976c00ec4c337d130757493b0
rmugattarov <mugattarov@gmail.com>
2017-05-22 16:16:28 +0300

    updates

---
+++ src/main/java/rmugattarov/cast/BooleanValueOfNull.java
@@ -0,0 +1,10 @@
+package rmugattarov.cast;
+
+/**
+ * Created by rmugattarov on 22.05.2017.
+ */
+public class BooleanValueOfNull {
+    public static void main(String[] args) {
+        System.out.println(Boolean.valueOf(null));
+    }
+}
