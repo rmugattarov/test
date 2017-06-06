91eb3eb68d1a8a2c5e60d91a545222066c05d183
rmugattarov <mugattarov@gmail.com>
2017-06-06 09:11:33 +0300

    updates

---
+++ src/main/java/rmugattarov/functions/FuncMutithreading.java
@@ -0,0 +1,35 @@
+package rmugattarov.functions;
+
+import java.util.function.Consumer;
+
+/**
+ * Created by rmugattarov on 06.06.2017.
+ */
+public class FuncMutithreading {
+    private static final Consumer<String> f = arg -> {
+        String s = "";
+        s += arg;
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + " : " + arg + " : " + s);
+    };
+
+    private static class Task implements Runnable {
+        @Override
+        public void run() {
+            f.accept(Thread.currentThread().getName());
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Task());
+        Thread t2 = new Thread(new Task());
+        Thread t3 = new Thread(new Task());
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
