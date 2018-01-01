51e5b24ee72ed6b31d657a2a69a8626d54c88973
rmugattarov <mugattarov@gmail.com>
2018-01-01 17:57:24 +0300

    updates

---
+++ src/main/java/rmugattarov/math/Modulus.java
@@ -0,0 +1,11 @@
+package rmugattarov.math;
+
+public class Modulus {
+    public static void main(String[] args) {
+        System.out.println(3 % 1);
+        System.out.println(3 % 2);
+        System.out.println(3 % 3);
+        System.out.println(3 % 4);
+        System.out.println(3 % 5);
+    }
+}
