a3269ad5d44b932f0e4a09a60d33ba6a541dbe53
rmugattarov <mugattarov@gmail.com>
2018-01-27 12:45:45 +0300

    updates

---
+++ src/main/java/rmugattarov/codility/MinimalNotOccurringInt.java
@@ -0,0 +1,17 @@
+package rmugattarov.codility;
+
+import java.util.Arrays;
+
+public class MinimalNotOccurringInt {
+    public static void main(String[] args) {
+        int[] A = {5,4,3,2,1,9,8,7};
+        Arrays.sort(A);
+        for (int i=0;i<A.length;i++){
+            int x = A[i];
+            if(x<=0){
+                continue;
+            } else if ()
+        }
+        return
+    }
+}
