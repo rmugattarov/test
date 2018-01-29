88381fa692f5d37ba46ea6e8f5c991b66f7b6e77
rmugattarov <mugattarov@gmail.com>
2018-01-29 17:50:32 +0300

    updates

---
+++ src/main/java/rmugattarov/autoboxing/ComparePrimitiveAndObject.java
@@ -0,0 +1,10 @@
+package rmugattarov.autoboxing;
+
+public class ComparePrimitiveAndObject {
+    public static void main(String[] args) {
+        Long l = 5L;
+        System.out.println(3L < l);
+        System.out.println(3L == l);
+        System.out.println(3L > l);
+    }
+}
--- src/main/java/rmugattarov/codility/MinimalNotOccurringInt.java
+++ src/main/java/rmugattarov/codility/MinimalNotOccurringInt.java
@@ -6,12 +6,12 @@ public class MinimalNotOccurringInt {
     public static void main(String[] args) {
         int[] A = {5,4,3,2,1,9,8,7};
         Arrays.sort(A);
-        for (int i=0;i<A.length;i++){
-            int x = A[i];
-            if(x<=0){
-                continue;
-            } else if ()
-        }
-        return
+//        for (int i=0;i<A.length;i++){
+//            int x = A[i];
+//            if(x<=0){
+//                continue;
+//            } else if ()
+//        }
+//        return
     }
 }
