db4d14d9780f1214ff418320d6d27a616395b17c
rmugattarov <mugattarov@gmail.com>
2018-10-08 21:29:17 +0300

    updates

--- src/main/java/rmugattarov/crack/ComplFuture.java
+++ src/main/java/rmugattarov/crack/ComplFuture.java
@@ -3,15 +3,18 @@ package rmugattarov.crack;
 import java.util.concurrent.CompletableFuture;
 
 public class ComplFuture {
+    private static int i = 0;
+
     public static void main(String[] args) {
         System.out.println("Start Thread: " + Thread.currentThread().getName());
         CompletableFuture.supplyAsync(() -> {
-            System.out.println("Hello Thread: " + Thread.currentThread().getName());
+            System.out.println("Hello Thread: i : " + i++ + " " + Thread.currentThread().getName());
             return "Hello!";
         }).thenAccept(s -> {
             System.out.println("Accepting Thread: " + Thread.currentThread().getName());
             System.out.println("Accepted: " + s);
         });
+        System.out.println("i : " + i + " Thread: " + Thread.currentThread().getName());
         System.out.println("End. Thread: " + Thread.currentThread().getName());
     }
 }
