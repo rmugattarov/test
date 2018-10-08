6bf8b1b39a97dc03446314d6ed8e7d20b7b2d656
rmugattarov <mugattarov@gmail.com>
2018-10-08 22:43:03 +0300

    updates

---
+++ src/main/java/rmugattarov/basics/y2018m10/IntMinMax.java
@@ -0,0 +1,10 @@
+package rmugattarov.basics.y2018m10;
+
+public class IntMinMax {
+    public static void main(String[] args) {
+        String maxIntString = Integer.toBinaryString(Integer.MAX_VALUE);
+        String minIntString = Integer.toBinaryString(Integer.MIN_VALUE);
+        System.out.println(maxIntString);
+        System.out.println(minIntString);
+    }
+}
