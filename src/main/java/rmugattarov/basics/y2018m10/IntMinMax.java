a31a1e5177cda32f09be15b62eb95f60fd44aa9f
rmugattarov <mugattarov@gmail.com>
2018-10-09 15:42:11 +0300

    updates

--- src/main/java/rmugattarov/basics/y2018m10/IntMinMax.java
+++ src/main/java/rmugattarov/basics/y2018m10/IntMinMax.java
@@ -4,7 +4,9 @@ public class IntMinMax {
     public static void main(String[] args) {
         String maxIntString = Integer.toBinaryString(Integer.MAX_VALUE);
         String minIntString = Integer.toBinaryString(Integer.MIN_VALUE);
+        String negOne = Integer.toBinaryString(-1);
         System.out.println(maxIntString);
         System.out.println(minIntString);
+        System.out.println(negOne);
     }
 }
