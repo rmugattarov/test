4684f6fa75427b8b92245b10fc8c714df07a1f30
Robert Mugattarov <mugattarov@gmail.com>
2019-01-31 19:57:42 +0300

    updates

--- src/main/java/rmugattarov/concurrency/LeftRightRobotExecService.java
+++ src/main/java/rmugattarov/concurrency/LeftRightRobotExecService.java
@@ -1,22 +1,36 @@
 package rmugattarov.concurrency;
 
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class LeftRightRobotExecService {
     private static final ExecutorService left = Executors.newFixedThreadPool(1);
     private static final ExecutorService right = Executors.newFixedThreadPool(1);
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
+        Semaphore s = new Semaphore(1);
         while (true) {
+            s.acquire();
             left.submit(() -> {
-                System.out.println("left");
-                return null;
-            }).get();
-            Thread.sleep(1000);
-            right.submit(() -> {
-                System.out.println("right");
-                return null;
-            }).get();
+                System.out.println("left " + Thread.currentThread().getName());
+                right.submit(() -> {
+                    System.out.println("right " + Thread.currentThread().getName());
+                    sleepRandomTime();
+                    s.release();
+                });
+                sleepRandomTime();
+            });
+
+        }
+    }
+
+    private static void sleepRandomTime() {
+        try {
+            Thread.sleep(ThreadLocalRandom.current().nextInt(2000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
