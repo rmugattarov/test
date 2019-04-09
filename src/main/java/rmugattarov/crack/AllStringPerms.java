24224bc675a96b1270a6c5ee9a55a2aadebe1065
Robert Mugattarov <mugattarov@gmail.com>
2019-04-09 14:54:38 +0300

    updates

--- src/main/java/rmugattarov/crack/AllStringPerms.java
+++ src/main/java/rmugattarov/crack/AllStringPerms.java
@@ -1,62 +1,23 @@
 package rmugattarov.crack;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
-
 public class AllStringPerms {
-    private static final Random R = ThreadLocalRandom.current();
 
     public static void main(String[] args) {
-        for (int i = 0; i < 1; i++) {
-//            String randomString = RandomStringUtils.random(R.nextInt(10), "abcdefghijklmnopqrstuvwxyz");
-//            String randomString = "123";
-//            System.out.println("Input string:");
-//            System.out.println(randomString);
-//            System.out.println("Permutations:");
-
-        }
-
-        char[][] mergeResult = mergePerms('c', new char[][]{{'a', 'b'}, {'1', '2'}});
-        for (char[] chars : mergeResult) {
-            System.out.println(chars);
-        }
+        permutation("abcde");
     }
 
+    private static void permutation(String str) {
+        permutation(str, "");
+    }
 
-    private static char[][] mergePerms(char c, char[][] perms) {
-        int resultLength = perms.length;
-        for (char[] perm : perms) {
-            resultLength += perm.length;
-        }
-        char[][] result = new char[resultLength][];
-
-        int resultIndex = 0;
-        for (char[] perm : perms) {
-            char[][] newPerms = mergePerm(c, perm);
-            for (char[] newPerm : newPerms) {
-                result[resultIndex++] = newPerm;
+    private static void permutation(String str, String prefix) {
+        if (str.length() == 0) {
+            System.out.println(prefix);
+        } else {
+            for (int i = 0; i < str.length(); i++) {
+                String rem = str.substring(0, i) + str.substring(i + 1);
+                permutation(rem, prefix + str.charAt(i));
             }
         }
-
-        return result;
-    }
-
-
-    private static char[][] mergePerm(char c, char[] chars) {
-        char[][] result = new char[chars.length + 1][];
-        char[] newPerm = new char[chars.length + 1];
-        newPerm[0] = c;
-        System.arraycopy(chars, 0, newPerm, 1, chars.length);
-        result[0] = newPerm;
-        int resultIndex = 1;
-        for (int j = 0; j < newPerm.length - 1; j++) {
-            newPerm = Arrays.copyOf(newPerm, newPerm.length);
-            char temp = newPerm[j];
-            newPerm[j] = newPerm[j + 1];
-            newPerm[j + 1] = temp;
-            result[resultIndex++] = newPerm;
-        }
-        return result;
     }
 }
