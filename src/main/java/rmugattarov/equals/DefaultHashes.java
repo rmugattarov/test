dd57bd5a9f601be3fe4adf0c9dd4783d0412b65c
Robert Mugattarov <mugattarov@gmail.com>
2019-04-15 14:56:10 +0300

    updates

---
+++ src/main/java/rmugattarov/equals/DefaultHashes.java
@@ -0,0 +1,18 @@
+package rmugattarov.equals;
+
+public class DefaultHashes {
+
+    public static void main(String[] args) {
+        System.out.println(new Object().hashCode());
+        System.out.println(new Object().hashCode());
+        System.out.println(new Object().hashCode());
+        System.out.println(new Object().hashCode());
+        System.out.println(new Object().hashCode());
+
+        System.out.println(new DefaultHashes().hashCode());
+        System.out.println(new DefaultHashes().hashCode());
+        System.out.println(new DefaultHashes().hashCode());
+        System.out.println(new DefaultHashes().hashCode());
+        System.out.println(new DefaultHashes().hashCode());
+    }
+}
