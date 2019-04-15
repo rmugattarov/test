0835cd3f6e501f16b2c1fce243153b876f02cecd
Robert Mugattarov <mugattarov@gmail.com>
2019-04-15 19:01:47 +0300

    updates

---
+++ src/main/java/rmugattarov/bitwise/Shifts.java
@@ -0,0 +1,12 @@
+package rmugattarov.bitwise;
+
+public class Shifts {
+    public static void main(String[] args) {
+        System.out.println("10 >> 1: " + (10 >> 1));
+        System.out.println("10 >>> 1: " + (10 >>> 1));
+        System.out.println("-10 >> 1: " + (-10 >> 1));
+        System.out.println("-10 >>> 1: " + (-10 >>> 1));
+        System.out.println("10 << 1: " + (10 << 1));
+        System.out.println("-10 << 1: " + (-10 << 1));
+    }
+}
