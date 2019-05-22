ad8b37a4d75598aaa0d124e19df49042ced0e4f6
Robert Mugattarov <mugattarov@gmail.com>
2019-05-22 15:27:24 +0300

    updates

--- src/main/java/rmugattarov/search/IntersectPostingLists.java
+++ src/main/java/rmugattarov/search/IntersectPostingLists.java
@@ -1,5 +1,6 @@
 package rmugattarov.search;
 
+import org.apache.commons.lang.ArrayUtils;
 import rmugattarov.random.RandomUtil;
 
 import java.util.Arrays;
@@ -11,11 +12,11 @@ public class IntersectPostingLists {
             int[] arr2 = RandomUtil.getRandomSortedUniqueElementsArray();
             System.out.println("1) " + Arrays.toString(arr1));
             System.out.println("2) " + Arrays.toString(arr2));
-            System.out.println(Arrays.toString(intersectPostingLists(arr1, arr2)));
+            System.out.println(Arrays.toString(disjunct(arr1, arr2)));
         }
     }
 
-    public static int[] intersectPostingLists(int[] leftPL, int[] rightPL) {
+    public static int[] conjunct(int[] leftPL, int[] rightPL) {
         int[] res = new int[Math.min(leftPL.length, rightPL.length)];
         int leftPointer = 0;
         int rightPointer = 0;
@@ -35,6 +36,43 @@ public class IntersectPostingLists {
             }
         }
 
-        return Arrays.copyOfRange(res, 0,resPointer);
+        return Arrays.copyOfRange(res, 0, resPointer);
     }
+
+    public static int[] disjunct(int[] leftPL, int[] rightPL) {
+        int[] res = new int[leftPL.length + rightPL.length];
+        int leftPointer = 0;
+        int rightPointer = 0;
+        int resPointer = 0;
+
+        while (leftPointer < leftPL.length && rightPointer < rightPL.length) {
+            int left = leftPL[leftPointer];
+            int right = rightPL[rightPointer];
+            if (left == right) {
+                res[resPointer++] = left;
+                leftPointer++;
+                rightPointer++;
+            } else if (left < right) {
+                res[resPointer++] = left;
+                leftPointer++;
+            } else {
+                res[resPointer++] = right;
+                rightPointer++;
+            }
+        }
+        if (leftPointer < leftPL.length) {
+            res = Arrays.copyOfRange(res, 0, resPointer);
+            int[] remaining = Arrays.copyOfRange(leftPL, leftPointer, leftPL.length);
+            return ArrayUtils.addAll(res, remaining);
+        }
+        if (rightPointer < rightPL.length) {
+            res = Arrays.copyOfRange(res, 0, resPointer);
+            int[] remaining = Arrays.copyOfRange(rightPL, rightPointer, rightPL.length);
+            return ArrayUtils.addAll(res, remaining);
+        }
+
+        return Arrays.copyOfRange(res, 0, resPointer);
+    }
+
+
 }
