7fdf9e83b8a5880eace193ac0f17449c8a04fad2
Robert Mugattarov <mugattarov@gmail.com>
2019-11-19 20:52:31 +0300

    updates

--- src/main/java/rmugattarov/yndx/Parenths1.java
+++ src/main/java/rmugattarov/yndx/Parenths1.java
@@ -1,7 +1,35 @@
 package rmugattarov.yndx;
 
-public class Parenths1 {
-    public static void main(String[] args) {
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Scanner;
 
+public class Parenths1 {
+    public static void main(String[] args) throws IOException {
+        char[] chars = new char[new Scanner(System.in).nextInt() * 2];
+        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("output.txt"))) {
+            parens(chars, 0, 0, out);
+        }
+    }
+
+    private static void parens(char[] chars, int open, int closed, OutputStream out) throws IOException {
+        int i = open + closed;
+        if (i == chars.length) {
+            for (char c : chars) {
+                out.write(c);
+            }
+            out.write(10);
+            return;
+        }
+        if (open < chars.length / 2) {
+            chars[i] = '(';
+            parens(chars, open + 1, closed, out);
+        }
+        if (closed < open) {
+            chars[i] = ')';
+            parens(chars, open, closed + 1, out);
+        }
     }
 }
