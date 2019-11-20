81312e41f5cd3f17eca05c2c69693ecc2fe6ea4b
Robert Mugattarov <mugattarov@gmail.com>
2019-11-20 11:31:42 +0300

    updates

--- src/main/java/rmugattarov/yndx/MergeSorted.java
+++ src/main/java/rmugattarov/yndx/MergeSorted.java
@@ -1,9 +1,6 @@
 package rmugattarov.yndx;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Arrays;
 
 public class MergeSorted {
@@ -19,18 +16,23 @@ public class MergeSorted {
         };
 
         for (String fileName : fileNames) {
-            try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(fileName))) {
-                getStatsAndPrint(in);
-                System.out.println();
+            try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(fileName));
+                 BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("output.txt"))) {
+                getStatsAndPrint(in, out);
             }
         }
+
+//        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream("input.txt"));
+//             BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("output.txt"))) {
+//            getStatsAndPrint(in, out);
+//        }
 //        try(BufferedInputStream in = new BufferedInputStream(new FileInputStream("input.txt"))) {
 //            getStatsAndPrint(in);
 //        }
 //        getStatsAndPrint(System.in);
     }
 
-    private static void getStatsAndPrint(InputStream in) throws IOException {
+    private static void getStatsAndPrint(InputStream in, OutputStream out) throws IOException {
         byte[] nextBytes = new byte[8];
         int k = nextInt(in, nextBytes);
         short[] stats = new short[101];
@@ -42,10 +44,19 @@ public class MergeSorted {
             }
         }
         for (byte i = 0; i < stats.length; i++) {
-            int val = stats[i];
+            short val = stats[i];
             while (val-- > 0) {
-                System.out.print(i);
-                System.out.print(' ');
+                if (i == 100) {
+                    out.write(49);
+                    out.write(48);
+                    out.write(48);
+                } else if (i > 9) {
+                    out.write(i / 10 + 48);
+                    out.write(i % 10 + 48);
+                } else {
+                    out.write(i + 48);
+                }
+                out.write(32);
             }
         }
     }
