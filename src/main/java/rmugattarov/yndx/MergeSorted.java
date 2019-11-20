801f59532f0b50eb7cedca3d3b585a3fa9ec29be
Robert Mugattarov <mugattarov@gmail.com>
2019-11-20 07:50:41 +0300

    updates

--- src/main/java/rmugattarov/yndx/MergeSorted.java
+++ src/main/java/rmugattarov/yndx/MergeSorted.java
@@ -1,33 +1,25 @@
 package rmugattarov.yndx;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 
 public class MergeSorted {
     public static void main(String[] args) throws IOException {
-//        Scanner sc = new Scanner("4\n" +
-//                "6 2 26 64 88 96 96\n" +
-//                "4 8 20 65 86\n" +
-//                "7 1 4 16 42 58 61 69\n" +
-//                "1 84");
-//        Scanner sc = new Scanner(System.in);
-//        try (FileInputStream fis = new FileInputStream("C:\\Users\\mugat\\IdeaProjects\\test\\src\\main\\java\\rmugattarov\\yndx\\input_merge_sorted.txt")) {
-        try (FileInputStream fis = new FileInputStream("input.txt")) {
-            int k = nextInt(fis);
-            int[] res = new int[0];
-            for (int i = 0; i < k; i++) {
-                int len = nextInt(fis);
-                int[] next = new int[len];
-                for (int j = 0; j < len; j++) {
-                    next[j] = nextInt(fis);
-                }
-                res = merge(res, next);
+        byte[] nextBytes = new byte[8];
+        int k = nextInt(System.in, nextBytes);
+        int[] res = new int[0];
+        for (int i = 0; i < k; i++) {
+            int len = nextInt(System.in, nextBytes);
+            int[] nextArr = new int[len];
+            for (int j = 0; j < len; j++) {
+                nextArr[j] = nextInt(System.in, nextBytes);
             }
-            for (int i = 0; i < res.length; i++) {
-                System.out.print(res[i]);
-                if (i < res.length - 1) System.out.print(' ');
-            }
+            res = merge(res, nextArr);
+        }
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i]);
+            System.out.print(' ');
         }
     }
 
@@ -49,20 +41,22 @@ public class MergeSorted {
         return res;
     }
 
-    private static int nextInt(InputStream is) throws IOException {
+    private static int nextInt(InputStream is, byte[] bytes) throws IOException {
+        Arrays.fill(bytes, (byte) 0);
         byte b;
         byte i = 0;
-        byte[] bytes = new byte[8];
         while (true) {
             b = (byte) is.read();
-            if (b == (byte) 10) continue;
-            if (b == (byte) 13) break;
-            if (b == (byte) -1) break;
-            if (b == (byte) 32) break;
+            if (i == 0 && (b < 48 || b > 57)) continue;
+            if (b < 48 || b > 57) break;
             bytes[i++] = b;
         }
-        byte[] res = new byte[i];
-        System.arraycopy(bytes, 0, res, 0, i);
-        return Integer.valueOf(new String(res).trim());
+        int res = 0;
+        int order = 1;
+        while (--i >= 0) {
+            res += (bytes[i] - 48) * order;
+            order *= 10;
+        }
+        return res;
     }
 }
