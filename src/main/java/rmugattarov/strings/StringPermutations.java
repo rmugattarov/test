52a4a6b86ec45a4821dc6ceea1bb9850c8dae82e
Robert Mugattarov <mugattarov@gmail.com>
2019-11-27 10:32:54 +0300

    updates

---
+++ src/main/java/rmugattarov/strings/StringPermutations.java
@@ -0,0 +1,23 @@
+package rmugattarov.strings;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class StringPermutations {
+    private static final Random R = ThreadLocalRandom.current();
+
+    public static void main(String[] args) {
+        String s = RandomStringUtils.random(R.nextInt(6), "abc");
+        printStringPermutations(new AtomicInteger(),"", "abcdef");
+    }
+
+    public static void printStringPermutations(AtomicInteger count, String pref, String post) {
+        if (post.length() == 0) System.out.println(count.incrementAndGet() + ") " + pref);
+        for (int i = 0; i < post.length(); i++) {
+            printStringPermutations(count,pref + post.charAt(i), post.substring(0, i) + post.substring(i + 1));
+        }
+    }
+}
