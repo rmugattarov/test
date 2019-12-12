70e9df5ca61124f58b10d3cf857ea5ff09c37e3f
Robert Mugattarov <mugattarov@gmail.com>
2019-12-12 12:01:32 +0300

    updates

---
+++ src/main/java/rmugattarov/concurrency/SyncMetAndUnsync.java
@@ -0,0 +1,37 @@
+package rmugattarov.concurrency;
+
+public class SyncMetAndUnsync {
+    public static void main(String[] args) throws InterruptedException {
+        SyncMetAndUnsync o = new SyncMetAndUnsync();
+        Thread t1 = new Thread(()->{
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            o.un();
+            o.un();
+        });
+        Thread t2 = new Thread(()->{
+            o.s();
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            o.s();
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+
+    public void un() {
+        System.out.println("un");
+    }
+
+    public synchronized void s() {
+        System.out.println("s");
+    }
+}
