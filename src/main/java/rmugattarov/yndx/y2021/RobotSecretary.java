fcc53636ad3ca0ed5888ed986852b1cbbbf36161
Robert Mugattarov <r.mugattarov@tinkoff.ru>
2021-09-21 14:17:06 +0300

    upd

--- src/main/java/rmugattarov/yndx/y2021/RobotSecretary.java
+++ src/main/java/rmugattarov/yndx/y2021/RobotSecretary.java
@@ -7,11 +7,27 @@ import java.nio.file.Paths;
 public class RobotSecretary {
 
     private static int sum = Integer.MAX_VALUE;
+    private static int spaces = 0;
+
     public static void main(String[] args) throws IOException {
-//        String input = Files.readAllLines(Paths.get("input.txt")).get(0);
+        String input = Files.readAllLines(Paths.get("input.txt")).get(0);
 //        String input = "Hello World";
-        String input = "APPLE II";
-        System.out.println(sum(0, input, true, 0));
+//        String input = "APPLE II";
+        input = removeSpaces(input);
+        System.out.println(spaces + sum(0, input, true, 0));
+    }
+
+    private static String removeSpaces(String input) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == ' ') {
+                spaces++;
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
     }
 
     private static int sum(int i, String input, boolean lowerReg, int sum) {
@@ -34,45 +50,3 @@ public class RobotSecretary {
     }
 
 }
-
-//    public static void single() throws IOException {
-////        String input = Files.readAllLines(Paths.get("input.txt")).get(0);
-//        String input = "Hello World";
-////        String input = "APPLE II";
-////        String input = "AAAaaAaaa";
-//        boolean lower = true;
-//        int lowerReps = 0;
-//        int upperReps = 0;
-//        int sum = 0;
-//        for (int i = 0; i < input.length(); i++) {
-//            char c = input.charAt(i);
-//
-//            if (c == ' ') {
-//                sum++;
-//                continue;
-//            }
-//
-//            if (lower && Character.isLowerCase(c)) {
-//                sum++;
-//            } else if (!lower && Character.isUpperCase(c)) {
-//                sum++;
-//            } else {
-//                sum += 2;
-//            }
-//
-//            if (Character.isLowerCase(c)) {
-//                upperReps = 0;
-//                lowerReps++;
-//                if (lowerReps == 3 && !lower && (i + 1 == input.length() || Character.isLowerCase(input.charAt(i + 1)))) {
-//                    lower = true;
-//                }
-//            } else if (Character.isUpperCase(c)) {
-//                lowerReps = 0;
-//                upperReps++;
-//                if (upperReps == 3 && lower && (i + 1 == input.length() || Character.isUpperCase(input.charAt(i + 1)))) {
-//                    lower = false;
-//                }
-//            }
-//        }
-//        System.out.println(sum);
-//    }
